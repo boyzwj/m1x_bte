5412ce74276d01ad14ce800760d0e11de0d2ca6f
Tom <tom@illusioncolors.com>
2022-11-07 20:24:34 +0800

    code optimize

--- view/element/node.py
+++ view/element/node.py
@@ -1,3 +1,4 @@
+from PySide6.QtGui import QColor
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
@@ -7,8 +8,10 @@ import PySide6
 class Node(QGraphicsRectItem):
     def __init__(self, parent=None) -> None:
         super(Node, self).__init__(parent)
-        self.setBrush(QBrush(QColor(0, 160, 230)))
-        self.setRect(0, 0, 100, 100)
+        self.brush: QBrush = QBrush()
+        self.brush.setColor(QColor(0, 160, 230))
+        self.setBrush(self.brush)
+        self.setRect(0, 0, 125, 50)
         self.startPos = None
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable |
                      QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
@@ -17,19 +20,19 @@ class Node(QGraphicsRectItem):
     #     print("fuck off")
     #     return super().mouseDoubleClickEvent(event)
 
-    # def mousePressEvent(self, event: PySide6.QtWidgets.QGraphicsSceneMouseEvent) -> None:
+    def mousePressEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+        if event.button() == Qt.MouseButton.RightButton:
+            print("xxxxxxxxxxxx")
+            self.startPos = event.pos()
 
-    #     if event.button() == Qt.MouseButton.MiddleButton:
-    #         print("xxxxxxxxxxxx")
-    #         self.startPos = event.pos()
+        return super().mousePressEvent(event)
 
-    #     return super().mousePressEvent(event)
+    def mouseMoveEvent(self, event):
+        if self.startPos is not None:
+            delta = self.startPos - event.pos()
+            print(delta)
 
-    # def mouseMoveEvent(self, event):
-    #     if self.startPos is not None and event.button() == Qt.MouseButton.RightButton:
-    #         delta = self.startPos - event.pos()
-    #         self.pos += delta
-
-    # def mouseReleaseEvent(self, event):
-    #     self.startPos = None
-    #     super(Node, self).mouseReleaseEvent(event)
+    def mouseReleaseEvent(self, event):
+        self.startPos = None
+        print("end mouse")
+        super(Node, self).mouseReleaseEvent(event)
